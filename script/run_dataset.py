a92a3298ddd39f14a1dd48dd37ab5d29cbdb8c90
BPYap <32807097+BPYap@users.noreply.github.com>
2019-09-17 18:28:46 +0800

    Revert "Modify tqdm progress bar"
    This reverts commit 1e3c0dee00c4833d69ce810515a14215dd450a04.

--- script/run_dataset.py
+++ script/run_dataset.py
@@ -28,7 +28,7 @@ from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler,
                               TensorDataset)
 from torch.utils.data.distributed import DistributedSampler
 from tensorboardX import SummaryWriter
-from tqdm.auto import tqdm
+from tqdm.auto import tqdm, trange
 
 from pytorch_transformers import (WEIGHTS_NAME, BertConfig,
                                   BertForSequenceClassification, BertTokenizer,
@@ -106,11 +106,10 @@ def train(args, train_dataset, model, tokenizer):
     global_step = 0
     tr_loss, logging_loss = 0.0, 0.0
     model.zero_grad()
-    train_iterator = range(int(args.num_train_epochs))
+    train_iterator = trange(int(args.num_train_epochs), desc="Epoch", disable=args.local_rank not in [-1, 0])
     set_seed(args)  # Added here for reproductibility (even between python 2 and 3)
-    for e in train_iterator:
-        epoch_iterator = tqdm(train_dataloader, desc=f"Epoch {e + 1}/{args.num_train_epochs}",
-                              disable=args.local_rank not in [-1, 0])
+    for _ in train_iterator:
+        epoch_iterator = tqdm(train_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0])
         for step, batch in enumerate(epoch_iterator):
             model.train()
             batch = tuple(t.to(args.device) for t in batch)
